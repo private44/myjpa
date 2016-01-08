88a59fa38c546194cb47fbb8b5289ccc0c819173
Sergiu Noroc <Sergiu.Noroc@endava.com>
2016-01-08 21:33:17 +0200

    add method addBook

--- src/com/myjpa/service/ServiceJPQL.java
+++ src/com/myjpa/service/ServiceJPQL.java
@@ -2,6 +2,7 @@
 package com.myjpa.service;
 
 import com.myjpa.domain.Employee;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -12,6 +13,21 @@ public class ServiceJPQL {
    
     private static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("DBOracle");
     private static final EntityManager EM = EMF.createEntityManager();
+    
+    public static void addBook(String firstName, String lastName, Date birthDate, String city, String gender, boolean active){
+ 
+        Employee employee = new Employee(); 
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employee.setBirthDate(birthDate);
+        employee.setCity(city);
+        employee.setGender(gender);
+        employee.setActive(active);
+        
+        EM.getTransaction().begin();
+        EM.persist(employee);
+        EM.getTransaction().commit();
+    }
     
     public static void getAllBooks(){
         Query query = EM.createQuery("select e from Employee e");
