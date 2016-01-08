a36cb8959bf67261d853acba52f77c8071a4a2f9
Sergiu Noroc <Sergiu.Noroc@endava.com>
2016-01-08 22:07:43 +0200

    add method getSelectedColumnEmployee()

--- src/com/myjpa/service/criteria/ServiceCriteria.java
+++ src/com/myjpa/service/criteria/ServiceCriteria.java
@@ -14,13 +14,23 @@ public class ServiceCriteria {
     private static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("DBOracle");
     private static final EntityManager EM = EMF.createEntityManager();
     
-    public static void getAllBooks(){
+    public static void getAllEmployee(){
         CriteriaBuilder cb = EM.getCriteriaBuilder();
         CriteriaQuery<Employee> cq = cb.createQuery(Employee.class);
         Root<Employee> emp = cq.from(Employee.class);
         cq.select(emp).where(cb.equal(emp.get("city"), "Chisinau"));
         
         TypedQuery<Employee> query = EM.createQuery(cq);
+        query.getResultList();
+    }
+    
+    public static void getSelectedColumnEmployee(){
+        CriteriaBuilder cb = EM.getCriteriaBuilder();
+        CriteriaQuery<Object[]> cq = cb.createQuery(Object[].class);
+        Root<Employee> emp = cq.from(Employee.class);
+        cq.multiselect(emp.get("firstName"), emp.get("lastName"));
+        
+        TypedQuery<Object[]> query = EM.createQuery(cq);
         query.getResultList();
     }
 }
