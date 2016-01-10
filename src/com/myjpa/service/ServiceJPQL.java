2947b33c20f0218b9eab8a88cf6a15a1238f6732
Sergiu Noroc <Sergiu.Noroc@endava.com>
2016-01-10 02:46:46 +0200

    add comment

--- src/com/myjpa/service/ServiceJPQL.java
+++ src/com/myjpa/service/ServiceJPQL.java
@@ -10,37 +10,38 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 public class ServiceJPQL {
-   
+
     private static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("DBOracle");
     private static final EntityManager EM = EMF.createEntityManager();
-    
+
     public static void addBook(String firstName, String lastName, Date birthDate, String city, String gender, boolean active){
- 
-        Employee employee = new Employee(); 
+
+        Employee employee = new Employee();
         employee.setFirstName(firstName);
         employee.setLastName(lastName);
         employee.setBirthDate(birthDate);
         employee.setCity(city);
         employee.setGender(gender);
         employee.setActive(active);
-        
+
+        //transaction in JavaSE    
         EM.getTransaction().begin();
         EM.persist(employee);
         EM.getTransaction().commit();
     }
-    
+
     public static void getAllEmployees(){
         Query query = EM.createQuery("select e from Employee e");
         List<Employee> results = query.getResultList();
-        
+
         for(Employee employee : results){
             System.out.println("First Name: " + employee.getFirstName());
         }
     }
-    
+
     public static void getTotalEmployees(){
         Query query = EM.createQuery("select count(e) from Employee e");
         Long result = (Long) query.getSingleResult();
-        System.out.println("Total employee: " + result); 
+        System.out.println("Total employee: " + result);
     }
 }
